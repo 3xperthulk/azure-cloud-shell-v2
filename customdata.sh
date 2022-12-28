e3a5396db9b105e26e12c010b7fa37fd1fcce40e
3xperthulk <41061831+3xperthulk@users.noreply.github.com>
2022-12-29 01:25:50 +0530

    Update and rename customdata.tpl to customdata.sh

--- customdata.sh
+++ customdata.sh
@@ -1,9 +1,11 @@
+#! /bin/bash
 sudo yum install -y yum-utils git
 sudo yum-config-manager     --add-repo     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo yum install java-1.8.0-openjdk.x86_64 -y
 sudo usermod -aG docker kafkaadmin
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo systemctl reboot
