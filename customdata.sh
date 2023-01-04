4db18f5e70f236bac03529829239feb0ac7628b6
Ayaan <ayaanmogal@gmail.com>
2023-01-04 15:06:16 +0530

    latest

--- customdata.sh
+++ customdata.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo sed -i '/^'"ClientAliveCountMax"'/d' /etc/ssh/sshd_config
-sudo echo "ClientAliveCountMax 170" >> /etc/ssh/sshd_config
+sudo sed -i '/^'"#ClientAliveCountMax"'/d' /etc/ssh/sshd_config
+sudo sh -c "echo 'ClientAliveCountMax 170' >> /etc/ssh/sshd_config"
 sudo systemctl restart sshd
 sudo yum install -y yum-utils git
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
